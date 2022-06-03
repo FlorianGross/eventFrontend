ec699a12779da99b63a780831fd256c208e258e6
Florian Groß <54940184+FlorianGross@users.noreply.github.com>
2022-06-04 00:50:48 +0200

    Worked on event create

--- src/services/event.service.js
+++ src/services/event.service.js
@@ -1,13 +1,53 @@
 import axios from 'axios'
 import authHeader from './auth-header'
-const API_URL = 'https://webprogevent.herokuapp.com/api/test/'
+const API_URL = 'https://webprogevent.herokuapp.com/api/event/'
 class UserService {
-  getPublicEvent() {
-    return axios.get(API_URL + 'publicEvent')
+  getEvent(id) {
+    return axios.post(API_URL + 'event', id, { headers: authHeader() })
   }
 
-  getUserEvent() {
-    return axios.get(API_URL + 'event', { headers: authHeader() })
+  createEvent(event) {
+    return axios.post(API_URL + 'create', {
+      creater: event.creater,
+      name: event.name,
+      description: event.description,
+      image: event.image,
+      location: event.location,
+      start: event.start,
+      end: event.end,
+      icon: event.icon,
+      maxParticipants: event.maxParticipants,
+      group: event.group,
+      cost: event.cost,
+    }, { headers: authHeader() })
+  }
+
+  updateEvent(event) {
+    return axios.post(API_URL + 'change', {
+      creater: event.creater,
+      name: event.name,
+      description: event.description,
+      image: event.image,
+      location: event.location,
+      start: event.start,
+      end: event.end,
+      icon: event.icon,
+      maxParticipants: event.maxParticipants,
+      group: event.group,
+      cost: event.cost,
+    }, { headers: authHeader() })
+  }
+
+  deleteEvent(id) {
+    return axios.post(API_URL + 'delete', id, { headers: authHeader() })
+  }
+
+  participateEvent(id, user) {
+    return axios.post(API_URL + 'participate', { id, user }, { headers: authHeader() })
+  }
+
+  unParticipateEvent(id, user) {
+    return axios.post(API_URL + 'unparticipate', { id, user }, { headers: authHeader() })
   }
 }
 export default new UserService()

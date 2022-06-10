9f7582c32d74c098d1dae369e263d0ff694fe98b
doe-eod <73845354+Doe-eod@users.noreply.github.com>
2022-06-10 16:44:08 +0200

    Impressum, Datenschutz

--- src/views/ImpressumView.vue
+++ src/views/ImpressumView.vue
@@ -1,17 +1,73 @@
 <template>
   <div class="body-of-impressum">
     <div class="impressumField">
-      <v-card style="height: 800px;">
+      <v-card style="height: 100%; margin-top: 9.4%;">
         <v-card-title>
           <span class="headline">Impressum</span>
         </v-card-title>
-        <v-card style="height: 350px">
+        <v-card>
+    <v-card-text class="impressumText"></v-card-text>
+    <h4>Florian Groß</h4>
+    <div
+      class="grid-containerPersonen"
+      style="margin-left: 2%; margin-bottom: 2%"
+    >
+      <img
+        class="rwu"
+        src="../assets/cos.jpg"
+      >
+        <div
+          class="text"
+          style="margin-left: 45%; margin-bottom: 5%"
+        >
+          <div>Vorname:</div>
+          <div>Nachname:</div>
+          <div>E-Mail:</div>
+          <div>Semester:</div>
+          </div>
+          <div class="text">
+            <div>Florian</div>
+            <div>Groß</div>
+            <div>florian.gross@hs-weingarten.de</div>
+            <div>4</div>
+          </div>
+          </div>
+  </v-card>
+  <v-card>
+    <v-card-text class="impressumText"></v-card-text>
+    <h4>Sven Herz</h4>
+    <div
+      class="grid-containerPersonen"
+      style="margin-top: 2%; margin-bottom: 2%"
+    >
+      <img
+        class="rwu"
+        src="../assets/bild3.jpg"
+      >
+        <div
+          class="text"
+          style="margin-left: 45%; margin-bottom: 5%"
+        >
+          <div>Vorname:</div>
+          <div>Nachname:</div>
+          <div>E-Mail:</div>
+          <div>Semester:</div>
+          </div>
+          <div class="text">
+            <div>Sven</div>
+            <div>Herz</div>
+            <div>sven.herz@hs-weingarten.de</div>
+            <div>4</div>
+          </div>
+          </div>
+  </v-card>
+        <v-card style="height: 1%">
           <v-card-text class="impressumText"></v-card-text>
           <h3>Hochschule Ravensburg-Weingarten</h3>
           <h3>University of Applied Sciences</h3>
           <div
             class="grid-containerRwu"
-            style="margin-top: 40px;"
+            style="margin-top: 2%; margin-bottom: 2%"
           >
             <img
               class="rwu"
@@ -19,12 +75,12 @@
             >
               <div
                 class="text"
-                style="margin-left: 250px;"
+                style="margin-left: 45%; margin-bottom: 5%"
               >
                 <div>Doggenriedstraße</div>
                 <div>88250 Weingarten</div>
                 <div>Deutschland</div>
-                <div style="margin-top: 15px;">Telefon: +49 (0) 751/501-9344</div>
+                <div style="margin-top: 5%;">Telefon: +49 (0) 751/501-9344</div>
                 <div>Telefax: +49 (0) 751/501-9876</div>
                 <div>E-Mail: info@rwu.de</div>
     </div>
@@ -42,62 +98,7 @@
     </div>
   </div>
   </v-card>
-  <v-card style="height: 200px">
-    <v-card-text class="impressumText"></v-card-text>
-    <h4>Florian Groß</h4>
-    <div
-      class="grid-containerPersonen"
-      style="margin-top: 20px;"
-    >
-      <img
-        class="rwu"
-        src="../assets/cos.jpg"
-      >
-        <div
-          class="text"
-          style="margin-left: 250px;"
-        >
-          <div>Vorname:</div>
-          <div>Nachname:</div>
-          <div>E-Mail:</div>
-          <div>Semester:</div>
-          </div>
-          <div class="text">
-            <div>Florian</div>
-            <div>Groß</div>
-            <div>florian.gross@hs-weingarten.de</div>
-            <div>4</div>
-          </div>
-          </div>
-  </v-card>
-  <v-card style="height: 220px">
-    <v-card-text class="impressumText"></v-card-text>
-    <h4>Sven Herz</h4>
-    <div
-      class="grid-containerPersonen"
-      style="margin-top: 20px;"
-    >
-      <img
-        class="rwu"
-        src="../assets/bild3.jpg"
-      >
-        <div
-          class="text"
-          style="margin-left: 250px;"
-        >
-          <div>Vorname:</div>
-          <div>Nachname:</div>
-          <div>E-Mail:</div>
-          <div>Semester:</div>
-          </div>
-          <div class="text">
-            <div>Sven</div>
-            <div>Herz</div>
-            <div>sven.herz@hs-weingarten.de</div>
-            <div>4</div>
-          </div>
-          </div>
-  </v-card>
+  
   </v-card>
   </div>
   </div>
@@ -114,7 +115,6 @@ export default {
   color: black;
   margin-left: auto;
   margin-right: auto;
-  margin-bottom: 0px;
 }
 .rwu {
   max-width: 100% !important;
@@ -124,19 +124,18 @@ export default {
 .text {
   color: #000000;
   font-size: small;
-  margin-top: 10px;
 }
 h3 {
-  margin-left: 20px;
+  margin-left: 2.5%;
   color: #6638b6;
 }
 h4 {
-  margin-left: 20px;
+  margin-left: 2%;
   color: #6638b6;
 }
 .impressumText {
   text-align: left;
-  margin-left: 10px;
+  margin-top: 0%;
 }
 .body-of-impressum {
   position: fixed;
@@ -153,27 +152,28 @@ h4 {
   background-repeat: no-repeat;
   background-size: cover;
   background-attachment: fixed;
+  overflow:auto;
 }
 .impressumField {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 1500px;
+  width: 96.4%;
   margin-left: auto;
   margin-right: auto;
   margin-top: auto;
 }
 .grid-containerRwu {
-  margin-top: 20px;
-  margin-left: 20px;
+  margin-top: 2%;
+  margin-left: 2%;
   display: grid;
-  grid-template-columns: 100px 600px 100px 300px;
+  grid-template-columns: 9% 40% 10% 100%;
 }
 .grid-containerPersonen {
-  margin-top: 20px;
-  margin-left: 20px;
+  margin-top: 2%;
+  margin-left: 2%;
   display: grid;
-  grid-template-columns: 100px 340px 300px;
+  grid-template-columns: 9% 20% 60%;
 }
 </style>
